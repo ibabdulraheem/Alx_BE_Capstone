ac7f99473f7181e9a82314b60e02449598f5aea2
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-29 22:36:20 +0100

    add router to the Api

--- Task_Management_Api/Task_Management_Api/settings.py
+++ Task_Management_Api/Task_Management_Api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'account',
+    'task',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Adding rest_framework settings
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES':[
+    'rest_framework.authentication.SessionAuthentication'
+    'rest_framework.authentication.TokenAuthentication'
+  ],
+  'DEFAULT_PERMISSION_CLASSES':[
+    'rest_framework.permissions.IsAuthenticated',
+  ]
+}
