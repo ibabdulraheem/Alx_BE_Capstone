dfeb2716f139f2fc9d24a6db7be44b5e4eed1dfe
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-30 21:33:10 +0100

    Addin Api endpoints for the task Ap

--- Task_Management_Api/accounts/serializers.py
+++ Task_Management_Api/accounts/serializers.py
@@ -1,16 +1,38 @@
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+# from django.contrib.auth.hashers import make_password
+
+
+
+# #creating serializer for user Registration
+# class RegisterSerializer(serializers.ModelSerializer):
+#   class meta:
+#     model = User
+#     fields = ['id','username','email','password']
+#     extra_kwargs = {'password':{'write_only':True}}
+
+#   def create(self,validated_data):
+#     validated_data['password'] = make_password(validated_data['password'])
+#     return User.objects.create(**validated_data)
+
+# accounts/serializers.py
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
 
 
-
-#creating serializer for user Registration
 class RegisterSerializer(serializers.ModelSerializer):
-  class meta:
-    model = User
-    fields = ['id','username','email','password']
-    extra_kwargs = {'password':{'write_only':True}}
+    password = serializers.CharField(write_only=True)
 
-    def create(self,validated_data):
-      validated_data['password'] = make_password(validated_data['password'])
-      return User.objects.create(**validated_data)
+    class Meta:   # 👈 this was missing
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        # Use Django's create_user to hash the password properly
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
