da5ea2dc5223f664cbe4dddda0eeb617a932cc25
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-30 10:56:19 +0100

    Adding reistration endpoint in Accounts App

--- Task_Management_Api/accounts/urls.py
+++ Task_Management_Api/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import RegistrationView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 
 
 
 #Registraion Endpoints
 urlpatterns = [
   path('register/',RegistrationView.as_view(), name = 'register'),
+  path('login/', TokenObtainPairView.as_view(), name='login'), 
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
