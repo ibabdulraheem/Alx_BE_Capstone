afaab83cfc688258e00a7653f8214f95702138e3
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-30 11:20:48 +0100

    Adding registration permission for user to register

--- Task_Management_Api/accounts/views.py
+++ Task_Management_Api/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,permissions
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 
@@ -8,4 +8,5 @@ from .serializers import RegisterSerializer
 class RegistrationView(generics.CreateAPIView):
   queryset = User.objects.all()
   serializer_class = RegisterSerializer
+  permission_classes = [permissions.AllowAny]
 
