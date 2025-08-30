fe22e10590e2c1928a147af979d176dc757d9b3e
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-30 10:40:18 +0100

    Adding reistration endpoint in Accounts App

---
+++ Task_Management_Api/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegistrationView
+
+
+
+#Registraion Endpoints
+urlpatterns = [
+  path('register/',RegistrationView.as_view(), name = 'register'),
+]
